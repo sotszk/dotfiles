1ee7a9f0422e253b0c5627bf46f5e86b7b559847
SotaSuzuki <3.3sota.suzuki@gmail.com>
2018-03-10 21:30:13 +0900

    added env-variables to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,7 @@
-# function fish_prompt
-#   set __fish_git_prompt_showstashstate 'yes'
-#   set __fish_git_prompt_showupstream 'yes'
-#   set __fish_git_prompt_showdirtystate 'yes'
-#   set __fish_git_prompt_color_branch yellow
-#   set __fish_git_prompt_char_dirtystate '⚡ '
-#   set -g fish_prompt_pwd_dir_length 0
-#   set_color red
-#   printf '[%s@%s]' (whoami) (prompt_hostname)
-#   set_color green
-#   printf ' %s' (__fish_git_prompt)
-#   set_color normal
-#   printf ' %s\n' (prompt_pwd)
-#   echo '% '
-# end
+set -x PATH "/usr/local/git/bin" $PATH
+set -x PATH "/usr/local/bin" $PATH
+set -x PATH "/usr/local/sbin" $PATH
+set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
+
+set -x PATH "$HOME/.anyenv/bin" $PATH
+# set -x PATH "$HOME/.anyenv/envs/ndenv/bin" $PATH
