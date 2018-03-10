3d48cd8ea7b6b93abcbdc4161e2be5d901e71b94
SotaSuzuki <3.3sota.suzuki@gmail.com>
2018-03-10 21:51:24 +0900

    removed all in fish config

--- fish/config.fish
+++
@@ -1,7 +0,0 @@
-set -x PATH "/usr/local/git/bin" $PATH
-set -x PATH "/usr/local/bin" $PATH
-set -x PATH "/usr/local/sbin" $PATH
-set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
-
-set -x PATH "$HOME/.anyenv/bin" $PATH
-# set -x PATH "$HOME/.anyenv/envs/ndenv/bin" $PATH
