58091bc057e371ff09762a92e7f268fc9ac41ad3
SotaSuzuki <3.3sota.suzuki@gmail.com>
2018-08-16 00:27:20 +0900

    Update hammerspoon config file

---
+++ .hammerspoon/command-kana.lua
@@ -0,0 +1,53 @@
+local simpleCmd = false
+local leftSet = false
+local rightSet = false
+
+local leftCmd = 0x37
+local rightCmd = 0x36
+local eisu = 0x66
+local kana = 0x68
+
+local function keyStroke(modifiers, character)
+    hs.eventtap.keyStroke(modifiers, character, 5000)
+end
+
+local function resetKeySet()
+    simpleCmd = false
+    leftSet = false
+    rightSet = false
+end
+ 
+local function eikanaEvent(event)
+    local c = event:getKeyCode()
+    local f = event:getFlags()
+    
+    if event:getType() == hs.eventtap.event.types.keyDown then
+        if f['cmd'] and c then
+            simpleCmd = true
+        end
+    elseif event:getType() == hs.eventtap.event.types.flagsChanged then
+        if f['cmd'] then
+            if c == leftCmd then
+                leftSet = true
+                rightSet = false
+            elseif c == rightCmd then
+                rightSet = true
+                leftSet = false
+            end
+        else
+            if simpleCmd == false then
+                if leftSet then
+                    keyStroke({}, eisu)
+                elseif rightSet then
+                    keyStroke({}, kana)
+                end
+            end
+            
+            resetKeySet()
+        end
+    end
+end
+
+eventtap = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged}, eikanaEvent)
+
+eventtap:start()
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,4 +1,5 @@
 require('watcher')
+-- require('command-kana') 
 
 local function keyCode(key, modifiers)
    modifiers = modifiers or {}
