f37b3091367fe2970855ad9c66d72dd1d3a7d7b2
SotaSuzuki <3.3sota.suzuki@gmail.com>
2018-08-17 14:44:11 +0900

    Add keybind to move word ending and beginning

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,48 +1,48 @@
 require('watcher')
--- require('command-kana') 
+-- require('command-kana')
 
 local function keyCode(key, modifiers)
-   modifiers = modifiers or {}
-   return function()
-      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
-      hs.timer.usleep(1000)
-      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
-   end
+  modifiers = modifiers or {}
+  return function()
+    hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+    hs.timer.usleep(1000)
+    hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+  end
 end
 
 local function keyCodeSet(keys)
-   return function()
-      for i, keyEvent in ipairs(keys) do
-         keyEvent()
-      end
-   end
+  return function()
+    for i, keyEvent in ipairs(keys) do
+      keyEvent()
+    end
+  end
 end
 
 local function remapKey(modifiers, key, keyCode)
-   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+  hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
 end
 
 local function disableAllHotkeys()
-   for k, v in pairs(hs.hotkey.getHotkeys()) do
-      v['_hk']:disable()
-   end
+  for k, v in pairs(hs.hotkey.getHotkeys()) do
+    v['_hk']:disable()
+  end
 end
 
 local function enableAllHotkeys()
-   for k, v in pairs(hs.hotkey.getHotkeys()) do
-      v['_hk']:enable()
-   end
+  for k, v in pairs(hs.hotkey.getHotkeys()) do
+    v['_hk']:enable()
+  end
 end
 
 local function handleGlobalAppEvent(name, event, app)
-   if event == hs.application.watcher.activated then
-      -- hs.alert.show(name)
-      if name == "iTerm2" then
-         disableAllHotkeys()
-      else
-         enableAllHotkeys()
-      end
-   end
+  if event == hs.application.watcher.activated then
+    -- hs.alert.show(name)
+    if name == "iTerm2" then
+      disableAllHotkeys()
+    else
+      enableAllHotkeys()
+    end
+  end
 end
 
 appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
@@ -63,3 +63,5 @@ remapKey({'ctrl'}, ']', keyCode('escape'))
 
 remapKey({'ctrl'}, 'v', keyCode('pagedown'))
 remapKey({'alt'}, 'v', keyCode('pageup'))
+remapKey({'alt'}, 'b', keyCode('left', {'alt'}))
+remapKey({'alt'}, 'f', keyCode('right', {'alt'}))
