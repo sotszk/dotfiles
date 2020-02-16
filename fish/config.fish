238318ec82ad03c243a777d866846e8e0e49516e
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-02-16 14:58:45 +0900

    add aliases to fish.config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,52 @@
 # removed welcome message
-function fish_greeting
+function greeting
+  echo 'Hello Fish! (git_current_branch)'
 end
+
+function reload
+  . ~/.ghq/github.com/SotaSuzuki/dotfiles/fish/config.fish
+end
+
+# git custom aliases
+alias gc 'git commit -m'
+alias gcam 'git commit --amend'
+alias gs 'git status'
+alias gp 'git pull'
+alias gf 'git fetch'
+alias gco 'git checkout'
+alias ga 'git add .'
+alias gpush 'git push'
+alias gd 'git diff'
+alias gb 'git branch'
+alias gr 'git reset'
+alias gr 'git reset --hard'
+
+# npm custom aliases
+alias ni 'npm install'
+alias nid 'npm install -D'
+alias nrs 'npm run start'
+alias nrd 'npm run dev'
+alias nrt 'npm run test'
+alias nrl 'npm run lint'
+alias nrb 'npm run build'
+alias npm-update 'npx npm-check -u'
+
+# yarn custom aliases
+alias y 'yarn'
+alias ya 'yarn add'
+alias yad 'yarn add -D'
+alias ys 'yarn start'
+alias yd 'yarn dev'
+alias yt 'yarn test'
+alias yl 'yarn lint'
+alias yb 'yarn build'
+alias yarn-update 'yarn upgrade-interactive --latest'
+
+# other custom aliases
+alias c 'code .'
+alias ll 'ls -1a'
+alias tree 'tree -NC'
+
+# company specific aliases
+alias cdweb 'ghq look yamap-web'
+alias cdeditor 'ghq look map-editor2'
